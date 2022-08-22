60f4684fe08b530a549454eb21400c38800fced5
Kerem Berkkanoğlu <berkkanoglukerem@gmail.com>
2022-08-22 19:59:50 +0300

    files changed

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 /*
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -13,12 +13,12 @@ root.render(
 */
 ReactDOM.render(
   <React.StrictMode>
-  
-      <App />
-  
+    <App />
   </React.StrictMode>,
   document.getElementById("root")
 );
+
+//react version update problem
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
